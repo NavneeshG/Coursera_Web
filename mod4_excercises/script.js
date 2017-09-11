f3d0d887d173477eb36ff3b8948c7d35ad10a8c3
NavneeshG <navneesh100@gmail.com>
2017-09-11 20:58:17 +0530

    latest dump

---
+++ mod4_excercises/script.js
@@ -0,0 +1,32 @@
+var x = "Hello Coursera.";
+/*function compare(x,y){
+	return x > y;
+	console.log(this);
+} 
+var a = compare (4,5);
+console.log(a);
+/*compare(4,a);
+compare();
+console.log(this);*/
+
+/*if ( false || NaN || undefined || "") {
+console.log( "To be never printed"); }
+else {
+console.log( "All evaluates to false");
+}
+
+if ( true && "false" && -1 )
+{
+	console.log("ALL EVALUATES TO TRUE");
+}*/
+
+var emp = {
+	name: "Navneesh",
+	md: { 
+		userid: "111",
+		type: "Extconsultant"
+	}
+};
+
+console.log(emp);
+
